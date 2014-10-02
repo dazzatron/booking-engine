cfab58bc6637ef4d8bbb23f4304b3a902e911413
jmditsolutions <joedarrington@hotmail.com>
2014-10-02 20:02:05 +0100

    bug fix: date re-selected on page reload fixed

--- app/controllers/date-selector.js
+++ app/controllers/date-selector.js
@@ -20,12 +20,29 @@
 
     // we get new data from server
     // if we have data before this, this will be called when created anyway, if we get it after it will also be called, so no issue
+    // can we replace this, so we only render view if theres data?
     var watcher = $scope.$watch('SharedResource.sharedData.trip.dates',
 
         function (newValue, oldValue) {
 
             if (newValue) {
-                $scope.changeDate(SharedResource.sharedData.trip.dates[0]);
+
+                var dateSelected = false;
+
+                for (var i = 0, length = SharedResource.sharedData.trip.dates.length; i < length; i++) {
+
+                    if (SharedResource.sharedData.trip.dates[i].selected) {
+                        dateSelected = true;
+                        break;
+                    };
+
+                }
+
+                // no date selected
+                if (!dateSelected) {
+                    $scope.changeDate(SharedResource.sharedData.trip.dates[0]);
+                }
+
                 watcher(); //removes
             }
 
