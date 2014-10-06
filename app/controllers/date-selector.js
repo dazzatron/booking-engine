a7c92b1797a5aeecdfcbbf9b10707c6e92277b77
jmditsolutions <joedarrington@hotmail.com>
2014-10-06 21:57:59 +0100

    improvement of data check

--- app/controllers/date-selector.js
+++ app/controllers/date-selector.js
@@ -18,32 +18,30 @@
 
     };
 
-    // we get new data from server
-    // if we have data before this, this will be called when created anyway, if we get it after it will also be called, so no issue
-    // can we replace this, so we only render view if theres data?
+    var checkDateSelected = function () {
+
+        for (var i = 0, length = SharedResource.sharedData.trip.dates.length; i < length; i++) {
+
+            if (SharedResource.sharedData.trip.dates[i].selected) {
+                return true;
+            }
+
+        }
+
+        return false;
+
+    };
+
+    // init    
     var watcher = $scope.$watch('SharedResource.sharedData.trip.dates',
 
         function (newValue, oldValue) {
 
-            if (newValue) {
+            if (newValue && !checkDateSelected()) {
 
-                var dateSelected = false;
-
-                for (var i = 0, length = SharedResource.sharedData.trip.dates.length; i < length; i++) {
-
-                    if (SharedResource.sharedData.trip.dates[i].selected) {
-                        dateSelected = true;
-                        break;
-                    }
-
-                }
-
-                // no date selected
-                if (!dateSelected) {
-                    $scope.changeDate(SharedResource.sharedData.trip.dates[0]);
-                }
-
+                $scope.changeDate(SharedResource.sharedData.trip.dates[0]);
                 watcher(); //removes
+
             }
 
         }, true);
